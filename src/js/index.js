aeece8f505be6b770ceaaf851cbf4aa9bdc8b99a
Leonel Badi <lbadi@itba.edu.ar>
2016-05-09 16:32:09 -0300

    Hello world with angular

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,21 @@
 (function () {
 
-  console.log('WOLOX');
+	console.log('FRIGE!');
 
 })();
+var module =angular.module('fridge', [ 'ngRoute']);
+
+module.config(function($routeProvider, $httpProvider ) {
+
+	$routeProvider.when('/', {
+		templateUrl : 'index.html',
+		controller : 'home',
+		controllerAs: 'controller'
+	}).otherwise('/')
+});
+
+module.controller('home', function(){
+	var self = this;
+
+	self.hello_world = "Hola heladera!!!";
+});
--- vendorJs.js
+++ vendorJs.js
@@ -1,2 +1,5 @@
 module.exports = [
+	"angular/angular.js",
+	"angular/angular.min.js.map",
+	"angular-route/angular-route.js"
 ];
